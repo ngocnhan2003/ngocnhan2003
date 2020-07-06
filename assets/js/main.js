174deee7bea315519b18c10ee43e5f9b333f4b5b
Nhan N. TRAN <ngocnhan.nplus@gmail.com>
2020-07-06 13:50:28 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,32 @@ $(function () {
 
     $('body').removeClass('is-preload');
 
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
     function load_insta(end_cursor = '') {
         let url = 'https://www.instagram.com/graphql/query/?query_hash=3e7706b09c6184d5eafd8b032dbcf487&variables={"tag_name":"ngocnhan2003","first":0,"after":"end_cursor"}';
         axios.get(url.replace('end_cursor', end_cursor))
             .then(function (response) {
                 let edge_hashtag_to_media = response.data.data.hashtag.edge_hashtag_to_media;
-                for (let edge of edge_hashtag_to_media.edges) {
+                let edges = shuffle(edge_hashtag_to_media.edges);
+                for (let edge of edges) {
                     let link = 'https://www.instagram.com/p/' + edge.node.shortcode;
                     $('#grid').append(`<li><img src="${edge.node.display_url}"></li>`);
                 }
