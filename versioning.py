c1a2269bebd54de99d71f3e47ffae168227507af
Nhan (Noah) N. TRAN <ngocnhan.nplus@gmail.com>
2021-06-27 12:51:46 +0700

    update base url

--- versioning.py
+++ versioning.py
@@ -3,9 +3,9 @@ import sys
 
 
 update_files = ['index.html']
-
+base_url = '/profile'
 commit_count = sys.argv[1]
-version_cdn = "https://cdn.jsdelivr.net/gh/ngocnhan2003/ngocnhan2003.github.io@v%s/assets/" % commit_count
+version_cdn = "https://cdn.jsdelivr.net/gh/ngocnhan2003/ngocnhan2003.github.io%s@v%s/assets/" % (base_url, commit_count)
 
 for update_file in update_files:
     index_html = open(update_file, 'r').read()
