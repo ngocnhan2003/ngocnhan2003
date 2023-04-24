810c0b79f2fbd8a92ed7ad5bffc2a69267eef2f0
ngocnhan2003 <ngocnhan.nplus@gmail.com>
2023-04-24 14:46:21 +0700

    update insta tag found

--- crawling.py
+++ crawling.py
@@ -45,5 +45,5 @@ except:
 
 if changed:
     print('write %d post to file: %s' % (len(result), instacrawjson))
-    open(instacrawjson, 'w').write(json.dumps({'tag_%s' % craw_tag : result}))
+    open(instacrawjson, 'w').write(json.dumps({'tag_%s' % craw_tag : result}, indent=4))
     print('done')
